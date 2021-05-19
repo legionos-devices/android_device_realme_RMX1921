1ceec417843a25592d4d6a7879711caa47d28ae1
ThatisJigen <gianfranco.liguori.96@gmail.com>
2021-05-19 12:50:15 +0200

    RMX1921: add back telephony to the boot jars

--- device.mk
+++ device.mk
@@ -451,8 +451,8 @@ PRODUCT_PACKAGES += \
     qti_telephony_utils.xml \
     telephony-ext
 
-#PRODUCT_BOOT_JARS += \
-#    telephony-ext
+PRODUCT_BOOT_JARS += \
+    telephony-ext
 
 # Touch
 PRODUCT_PACKAGES += \
